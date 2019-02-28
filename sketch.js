c72ad72cf79394b7a23fb45d6df26a047b76e37c
NoNelso <jasonr4nd@gmail.com>
2019-02-28 13:47:17 -0500

    created framework to use cell obj

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
+let x, y;
+let maze = [];
+
 function setup() {
   createCanvas(600, 600);
+  for (x = 0; x < width / 10; ++x)
+    for (y = 0; y < height / 10; ++y) {
+      let cell = new Cell(x, y);
+      maze.push(cell);
+    }
 }
 
 function draw() {
+  background(1);
+  for (let i = 0; i < maze.length; ++i) maze[i].show();
 
 }
 
