5f7266766e02558f410ae6f6c1f91efb3761d0f0
NoNelso <jasonr4nd@gmail.com>
2019-02-28 20:31:01 -0500

    swapped x,y array to descrete vars

--- cell.js
+++ cell.js
@@ -1,11 +1,12 @@
 function Cell(x, y) {
-  this.loc = [x, y]; //location
-  this.wal = [true, true, true, true] //walls U,R,D,L
+  this.x = x;
+  this.y = y;
+  this.wal = [true, true, true, true]; //walls U,R,D,L
   this.viz = false;
 
   this.show = function() {
-    let xw = this.loc[0] * w;
-    let yw = this.loc[1] * w;
+    let xw = this.x * w;
+    let yw = this.y * w;
     // U, R, D, L
     if (this.wal[0]) line(xw, yw, xw + w, yw);
     if (this.wal[1]) line(xw + w, yw, xw + w, yw + w);
