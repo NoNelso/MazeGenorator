2e35b9925e7e96ecba78bd1ec9b466fad102b7ce
NoNelso <jasonr4nd@gmail.com>
2019-02-28 20:37:22 -0500

    start 10.3 vid

--- sketch.js
+++ sketch.js
@@ -36,17 +36,17 @@ this.index = function(x, y) {
   }
   return x + (y * c);
 }
-// part 2
+//part3
 //psudo-code
 /*
-Make the initial cell the current cell and mark it as visited
-While there are unvisited cells
-  If the current cell has any neighbours which have not been visited
-    Choose randomly one of the unvisited neighbours
-    Push the current cell to the stack
-    Remove the wall between the current cell and the chosen cell
-    Make the chosen cell the current cell and mark it as visited
-  Else if stack is not empty
+Make the initial cell the current cell and mark it as visited           √
+While there are unvisited cells                                         ·
+  If the current cell has any neighbours which have not been visited    ·
+    Choose randomly one of the unvisited neighbours                     √
+    Push the current cell to the stack                                  x
+    Remove the wall between the current cell and the chosen cell        x
+    Make the chosen cell the current cell and mark it as visited        √
+  Else if stack is not empty                                            x
     Pop a cell from the stack
-    Make it the current cell
+    Make it the current cell                                            
 */
