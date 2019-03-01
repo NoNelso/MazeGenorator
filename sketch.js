ecebeb6b20ecea3930dc6e306b29bf34132971cf
NoNelso <jasonr4nd@gmail.com>
2019-02-28 23:40:24 -0500

    edited psudocode

--- sketch.js
+++ sketch.js
@@ -61,14 +61,14 @@ function removeWalls(c, n) {
 //part3
 //psudo-code
 /*
-Make the initial cell the current cell and mark it as visited           √
-While there are unvisited cells                                         ·
-  If the current cell has any neighbours which have not been visited    ·
-    Choose randomly one of the unvisited neighbours                     √
-    Push the current cell to the stack                                  x
-    Remove the wall between the current cell and the chosen cell        x
-    Make the chosen cell the current cell and mark it as visited        √
-  Else if stack is not empty                                            x
-    Pop a cell from the stack
-    Make it the current cell                                            
+1 Make the initial cell the current cell and mark it as visited           √
+2 While there are unvisited cells                                         ·
+  1 If the current cell has any neighbours which have not been visited    ·
+    1 Choose randomly one of the unvisited neighbours                     √
+    2 Push the current cell to the stack                                  x
+    3 Remove the wall between the current cell and the chosen cell        √
+    4 Make the chosen cell the current cell and mark it as visited        √
+  2 Else if stack is not empty                                            x
+    1 Pop a cell from the stack
+    2 Make it the current cell
 */
