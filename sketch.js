aac54a041677073d2e79dff2ea9b4b42a6649a89
NoNelso <jasonr4nd@gmail.com>
2019-03-01 00:13:27 -0500

    working copy of challenge

--- sketch.js
+++ sketch.js
@@ -64,17 +64,17 @@ function removeWalls(c, n) {
     n.wal[0] = false;
   }
 }
-//part4
+//frills
 //psudo-code
 /*
 1 Make the initial cell the current cell and mark it as visited           √
-2 While there are unvisited cells                                         ·
-  1 If the current cell has any neighbours which have not been visited    ·
+2 While there are unvisited cells                                         √
+  1 If the current cell has any neighbours which have not been visited    √
     1 Choose randomly one of the unvisited neighbours                     √
-    2 Push the current cell to the stack                                  x
+    2 Push the current cell to the stack                                  √
     3 Remove the wall between the current cell and the chosen cell        √
     4 Make the chosen cell the current cell and mark it as visited        √
-  2 Else if stack is not empty                                            x
-    1 Pop a cell from the stack
-    2 Make it the current cell
+  2 Else if stack is not empty                                            √
+    1 Pop a cell from the stack                                           √
+    2 Make it the current cell                                            √
 */
